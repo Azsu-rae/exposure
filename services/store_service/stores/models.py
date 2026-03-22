8f551fc7b42691a1bd1b43c14c5c15fb03b85e4f
Azsu-rae <aitameurmedilyas@gmail.com>
2026-03-22 16:24:08 +0100

    starting out

--- services/store_service/stores/models.py
+++ services/store_service/stores/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Store(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    city = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return (
+            f"({self.name}: {self.description} "
+            + f"Created at {self.created_at} in {self.city})"
+        )
---
+++ services/store_service/stores/scripts/test_db.py
@@ -0,0 +1,11 @@
+from stores.models import Store
+from django.db import models
+
+store = Store(
+    name="Titanic",
+    description="Expensive coffee shop",
+    city="Rouiba",
+)
+
+print(store)
+print("hello??")
