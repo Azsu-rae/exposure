c9cc3a447ad4ffe227c10409ae07591f2b915fc0
Azsu-rae <aitameurmedilyas@gmail.com>
2026-04-29 01:34:35 +0100

    settings cleanup, .env, and postgresql

---
+++ services/user_service/users/management/commands/test_db.py
@@ -0,0 +1,10 @@
+from django.core.management import BaseCommand
+
+from django.db import connection
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **kwargs):
+        cursor = connection.cursor()
+        cursor.execute("SELECT 1;")
+        print(cursor.fetchone())
